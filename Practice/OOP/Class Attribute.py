803f4b5ca2e41464f326b87e49deb13071e32b48
Bhavika300906 <227880734+Bhavika300906@users.noreply.github.com>
2026-04-16 11:17:19 +0530

    Class Attribute 16-04-26

---
+++ Practice/OOP/Class Attribute.py
@@ -0,0 +1,15 @@
+class Car:
+    wheels = 4
+
+
+    def __i
+    nit__(self,brand,model):
+        self.brand=brand
+        self.model=model
+
+
+car1= Car("Toyota", "Camry")
+car2= Car("Honda", "Civic")
+
+print(car1.wheels)
+print(car2.wheels)
---
+++ Practice/OOP/Static_method.py
@@ -0,0 +1,16 @@
+class Calculator:
+
+    @staticmethod
+    def add(x,y):
+        return x+y
+
+
+    @staticmethod
+    def multiply(x,y):
+        return x * y
+
+
+    #csllin static method
+print(Calculator.add(5,3))
+
+print(Calculator.multiply(4,6))
