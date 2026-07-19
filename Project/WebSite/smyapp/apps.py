ce9e63fe08db61b4c0cc80d646c3a59fb0eac633
Bhavika300906 <227880734+Bhavika300906@users.noreply.github.com>
2026-07-19 19:38:16 +0530

    Backend

--- Project/WebSite/smyapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SmyappConfig(AppConfig):
-    name = 'smyapp'
--- Project/WebSite/smyapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('',views.index,name='index'),
-    #1 conatact is show in url, 2nd view.contact show in views of the name, 3rd contact is match with thw header page name if not match then nothing show
-    path('contact/',views.contact,name='contact'),
-    path('signup/',views.signup,name='signup'), 
-    path('login/',views.login,name='login'),
-]
